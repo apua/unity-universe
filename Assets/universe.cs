501f7885cb928fef834eb0e839847cb3facb13bb
Apua Juan <apua.juan@sifive.com>
2023-12-23 20:15:22 +0800

    Implement `CreateChildSphere`

--- Assets/universe.cs
+++ Assets/universe.cs
@@ -1,18 +1,69 @@
-using System.Collections;
-using System.Collections.Generic;
+// Here is, after a few weeks, IMO the scope of this little project.
+//
+// - Eventually store and document "how to build it" on GitHub.
+// - Simply a rotating sphere.
+// - Light source might be an issue, and not been solved currently.
+//
+// Ref: "How to set up a Unity project in GitHub"
+// https://unityatscale.com/unity-version-control-guide/how-to-setup-unity-project-on-github/
+//
+// Future works:
+//
+// - Have a GUI to control it
+//   Ref: https://docs.unity3d.com/Manual/GUIScriptingGuide.html
+//
+// - Walk around and look around the space.
+// - Make the space more like a Universe.
+
 using UnityEngine;
 
-public class universe : MonoBehaviour
-{
+
+// `nullable` is a Swift Optional type.
+// `#` is a "preprocessor directives".
+#nullable enable
+
+// Define the object behavior.
+public class universe : MonoBehaviour {
+
+    // Must declare propertiese before using them. Unlike Python.
+    Vector3Int rotationAxis = new Vector3Int(60, 80, 0);
+
+    // The public properties will be interactive in Unity object inspector.
+    // Name in camelCase to gracefully display on field name of Unity object inspector.
+    public int degreesPerSecond = 36;
+    // NOTE: "property with setter" would not reflect to inspector.
+    // An alternative might be `MonoBehaviour.OnValidate`.
+
+    // Constants won't be interactiv in Unity object inspector even if public.
+    const int drawAreaWidth = 20;
+    const double starsRadiusRatio = 0.9f;
+    const double shapeRadius = drawAreaWidth * 0.5 * starsRadiusRatio;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        CreateChildSphere(Vector3Int.zero, Color.yellow, name: "Center");
+        CreateChildSphere(new Vector3(+1, 0, 0), Color.black);
+        CreateChildSphere(new Vector3(-1, 0, 0), Color.clear);
+        CreateChildSphere(new Vector3(0, +1, 0), Color.magenta);
+        CreateChildSphere(new Vector3(0, -1, 0), Color.magenta);
+        CreateChildSphere(new Vector3(0, 0, +1), Color.cyan);
+        CreateChildSphere(new Vector3(0, 0, -1), Color.cyan);
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
+    }
+
+    /* ******************** */
+
+    GameObject CreateChildSphere(Vector3 position, Color color, string? name = null) {
+        var obj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        // transform === gameObject.transform
+        obj.transform.parent = transform;
+        obj.name = name ?? $"Star-{transform.childCount}";
+        obj.transform.localPosition = position;
+        obj.GetComponent<Renderer>().material.SetColor("_Color", color);
+        return obj;
     }
 }
