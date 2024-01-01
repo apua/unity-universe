df1daa6637511ce484d506d20c1f78e6bd1c1852
Apua Juan <apua.juan@sifive.com>
2024-01-01 20:01:49 +0800

    Implement color changing

--- Assets/Universe.cs
+++ Assets/Universe.cs
@@ -1,69 +1,86 @@
-// Here is, after a few weeks, IMO the scope of this little project.
-//
-// - Eventually store and document "how to build it" on GitHub.
-// - Simply a rotating sphere.
-// - Light source might be an issue, and not been solved currently.
-//
-// Ref: "How to set up a Unity project in GitHub"
-// https://unityatscale.com/unity-version-control-guide/how-to-setup-unity-project-on-github/
-//
-// Future works:
-//
-// - Have a GUI to control it
-//   Ref: https://docs.unity3d.com/Manual/GUIScriptingGuide.html
-//
-// - Walk around and look around the space.
-// - Make the space more like a Universe.
-//
-// Other related:
-//
-// - `MonoBehaviour.InvokeRepeating`
-// - `Time.timeScale`
-// - `MonoBehaviour.Awake`
-// - `MonoBehaviour.OnValidate`
-
 using UnityEngine;
 
-// Define the object behavior.
 public class Universe : MonoBehaviour {
 
-    // * Must declare propertiese before using them. Unlike Python.
-    // * The public properties will be interactive in Unity object inspector.
-    public GameObject StarPrototype = default!;
-    public GameObject Stars; // = default!;
-    public int DegreesPerSecond = 36;
+    GameObject _prototype, _stars;
+    Color _color = new(0, 0, 1, 0.5f);
+
+    const ushort InitialAmount = 42;
+
+    public int DegreePerSecond = 36;
     public Vector3Int RotationAxis = new(60, 80, 0);
-    // NOTE: "property with setter" would not reflect to inspector; an alternative might be `MonoBehaviour.OnValidate`.
 
-    // * Constants won't be interactiv in Unity object inspector even if public.
-    const int DrawAreaWidth = 20;
-    const double StarsRadiusRatio = 0.9f;
-    const double ShapeRadius = DrawAreaWidth * 0.5 * StarsRadiusRatio;
-
-    // Start is called before the first frame update
     void Start() {
-        CreateChildSphere(+1, 0, 0, Color.black);
-        CreateChildSphere(-1, 0, 0, Color.clear);
-        CreateChildSphere(0, +1, 0, Color.magenta);
-        CreateChildSphere(0, -1, 0, Color.magenta);
-        CreateChildSphere(0, 0, +1, Color.cyan);
-        CreateChildSphere(0, 0, -1, Color.cyan);
+        _prototype = transform.Find("StarPrototype").gameObject;
+        _stars = transform.Find("Stars").gameObject;
+        for (int i = 0; i < InitialAmount; i++) AddStar();
     }
 
-    // Update is called once per frame
     void Update() {
-        Stars.transform.Rotate(axis: RotationAxis, angle: DegreesPerSecond * Time.deltaTime);
+        // Rotate.
+        _stars.transform.Rotate(axis: RotationAxis, angle: DegreePerSecond * Time.deltaTime);
+        // Color.
+        UpdateCurrentColor();
+        foreach (Transform starTransform in _stars.transform) {
+            starTransform.gameObject.GetComponent<Renderer>().material.color = _color;
+        }
     }
 
     /* ******************** */
 
-    void CreateChildSphere(int x, int y, int z, Color color) {
-        //var obj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        var obj = Instantiate(StarPrototype);
-        obj.transform.parent = Stars.transform;
-        obj.name = $"Star-{Stars.transform.childCount}";
-        obj.transform.localPosition = new Vector3Int(x, y, z);
-        obj.GetComponent<Renderer>().material.SetColor("_Color", color);
+    void AddStar() {
+        // New.
+        var obj = Instantiate(_prototype);
+        // Set parent to `Stars`.
+        obj.transform.parent = _stars.transform;
+        // Name "Star-{N}".
+        obj.name = $"Star-{_stars.transform.childCount}";
+        // Color.
+        obj.GetComponent<Renderer>().material.color = _color;
+        // Set random position.
+        obj.transform.localPosition = PointGenerators.Ring();
+        //obj.transform.localPosition = PointGenerators.Sphere();
+        // Enable.
         obj.SetActive(true);
+    }
+
+    void UpdateCurrentColor() {
+        // Change color:
+        // B   -> G   -> R   -> B i.e.
+        // 001 -> 010 -> 100 -> 001 i.e.
+        // 0   -> 1   -> 2   -> 3
+        // 0 -> 1 taks 2.5 seconds; per second is 0 -> 0.4
+        var colorAcc = Time.time * 0.4f % 3;
+        if (colorAcc <= 1) {
+            var x = colorAcc;
+            _color.r = 0; _color.g = x; _color.b = 1 - x;
+        } else if (colorAcc <= 2) {
+            var x = colorAcc - 1;
+            _color.b = 0; _color.r = x; _color.g = 1 - x;
+        } else {
+            var x = colorAcc - 2;
+            _color.g = 0; _color.b = x; _color.r = 1 - x;
+        }
+    }
+}
+
+class PointGenerators {
+    const ushort Radius = 10;
+
+    public static Vector3 Sphere() {
+        var θ = Random.value * 2 * Mathf.PI;
+        var φ = Mathf.Sqrt(Random.value) * Mathf.PI / 2;
+        return new(
+            Radius * Mathf.Sin(φ) * Mathf.Cos(θ),
+            Radius * Mathf.Cos(φ) * (Random.value > 0.5 ? 1 : -1),
+            Radius * Mathf.Sin(φ) * Mathf.Sin(θ));
+    }
+
+    public static Vector3 Ring() {
+        var θ = Random.value * 2 * Mathf.PI;
+        return new(
+            Radius * Mathf.Cos(θ),
+            0,
+            Radius * Mathf.Sin(θ));
     }
 }
