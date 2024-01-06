b68f0952b387220f8323dc164cb304bfa5118143
Apua Juan <apua.juan@sifive.com>
2024-01-06 14:49:50 +0800

    Set shape to enum value

--- Assets/Control.cs
+++ Assets/Control.cs
@@ -15,18 +15,11 @@ public class Control : MonoBehaviour
         root.Q("add").RegisterCallback<ClickEvent>(evt => universe.amount += (ushort)(universe.amount < 65535 ? 1 : 0));
         root.Q("del").RegisterCallback<ClickEvent>(evt => universe.amount -= (ushort)(universe.amount > 0     ? 1 : 0));
 
-        string[] shapeNames = {
-            "sphere",
-            "ring",
-            "2ring",
-            "cage",
-            "2cube",
-            "2sphere",
-            "tetrahedron",
-            "escherian-knot",
-        };
-        foreach (var name in shapeNames)
-            root.Q(name).RegisterCallback<ClickEvent>(evt => universe.SetShape(name));
+        foreach (var name in new string[] { "sphere", "ring", "2ring", "cage", "2cube", "2sphere", "tetrahedron", "escherian-knot" })
+            root.Q(name).RegisterCallback<ClickEvent>(evt => universe.Shape = name switch {
+                "sphere" => Shapes.Sphere,
+                _ => Shapes.Ring,
+            });
     }
 
     public void SetAmount(int amount)
