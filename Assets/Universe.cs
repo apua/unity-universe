f3f83f3da5defea1d7d2967a6af425e5c7c9599a
Apua Juan <apua.juan@sifive.com>
2024-01-06 09:38:51 +0800

    Refactor updating color

--- Assets/Universe.cs
+++ Assets/Universe.cs
@@ -23,7 +23,6 @@ public class Universe : MonoBehaviour
     public ushort amount;
     public Shapes Shape;
 
-    Color _color = new(0, 0, 1);
     Vector3[] _finalPositions;
     Vector3[] _velocities;
     float _transformShapeDelay = 0f;
@@ -32,18 +31,15 @@ public class Universe : MonoBehaviour
 
     void Start()
     {
-        UpdateAmount();
+        AdjustQuantity();
+        SetColor();
     }
 
     void Update()
     {
-        UpdateAmount();
-        // Rotate.
-        stars.transform.Rotate(axis: RotationAxis, angle: DegreePerSecond * Time.deltaTime);
-        // Color.
-        UpdateCurrentColor();
-        foreach (Transform starTransform in stars.transform)
-            starTransform.gameObject.GetComponent<Renderer>().material.color = _color;
+        AdjustQuantity();
+        SetColor();
+        Rotate();
         // Transform.
         if (_transformShapeDelay > 0)
             if (_transformShapeDelay > Time.deltaTime)
@@ -61,7 +57,7 @@ public class Universe : MonoBehaviour
             }
     }
 
-    void UpdateAmount()
+    void AdjustQuantity()
     {
         var count = stars.transform.childCount;
         if (amount > count)
@@ -76,6 +72,38 @@ public class Universe : MonoBehaviour
         }
     }
 
+    void SetColor()
+    {
+        // Change color:
+        // B   -> G   -> R   -> B i.e.
+        // 001 -> 010 -> 100 -> 001 i.e.
+        // 0   -> 1   -> 2   -> 3
+        // 0 -> 1 taks 2.5 seconds; per second is 0 -> 0.4
+        var t = Time.time * 0.4f % 3;
+        Color color;
+        if (t <= 1)
+        {
+            color = new(0, t, 1 - t);
+        }
+        else if (t <= 2)
+        {
+            t -= 1;
+            color = new(t, 1 - t, 0);
+        }
+        else
+        {
+            t -= 2;
+            color = new(1 - t, 0, t);
+        }
+        foreach (Transform starTransform in stars.transform)
+            starTransform.gameObject.GetComponent<Renderer>().material.color = color;
+    }
+
+    void Rotate()
+    {
+        stars.transform.Rotate(axis: RotationAxis, angle: DegreePerSecond * Time.deltaTime);
+    }
+
     /* ******************** */
 
     public void AddStar()
@@ -86,8 +114,6 @@ public class Universe : MonoBehaviour
         obj.transform.parent = stars.transform;
         // Name "Star-{N}".
         obj.name = $"Star-{stars.transform.childCount}";
-        // Color.
-        obj.GetComponent<Renderer>().material.color = _color;
         // Set random position.
         //obj.transform.localPosition = PointGenerators.Ring();
         obj.transform.localPosition = PointGenerators.Sphere();
@@ -99,31 +125,6 @@ public class Universe : MonoBehaviour
     {
         var index = stars.transform.childCount - 1;
         Destroy(stars.transform.GetChild(index).gameObject);
-    }
-
-    void UpdateCurrentColor()
-    {
-        // Change color:
-        // B   -> G   -> R   -> B i.e.
-        // 001 -> 010 -> 100 -> 001 i.e.
-        // 0   -> 1   -> 2   -> 3
-        // 0 -> 1 taks 2.5 seconds; per second is 0 -> 0.4
-        var colorAcc = Time.time * 0.4f % 3;
-        if (colorAcc <= 1)
-        {
-            var x = colorAcc;
-            _color.r = 0; _color.g = x; _color.b = 1 - x;
-        }
-        else if (colorAcc <= 2)
-        {
-            var x = colorAcc - 1;
-            _color.b = 0; _color.r = x; _color.g = 1 - x;
-        }
-        else
-        {
-            var x = colorAcc - 2;
-            _color.g = 0; _color.b = x; _color.r = 1 - x;
-        }
     }
 
     public void SetShape(string name)
